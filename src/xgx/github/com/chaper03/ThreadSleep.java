41790e97cc3d7bad7d9179a74c83535f4249fdd6
xiaoguixin <xiaoguixin@efunbox.cn>
2019-07-17 18:43:04 +0800

    init

---
+++ src/xgx/github/com/chaper03/ThreadSleep.java
@@ -0,0 +1,13 @@
+package xgx.github.com.chaper03;
+
+/**
+ * sleep方法介绍
+ */
+public class ThreadSleep {
+
+    public static void main(String[] args) {
+        new Thread(()->{
+
+        });
+    }
+}
